81c427fa46e3dca3e42bcc7cfc8279230c6d14a2
Noah Leigh <noah.d.leigh@gmail.com>
2018-01-31 10:10:57 -0500

    Added sessionStorage feature detection

--- src/utils.js
+++ src/utils.js
@@ -115,6 +115,35 @@ const getNextIndex = (index, array) => {
   }
 }
 
+/**
+ * Feature detection for the Web Storage API
+ * Source: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Feature-detecting_localStorage
+ * @param {String} type
+ */
+const storageAvailable = (type) => {
+  let storage = null
+  try {
+    storage = window[type]
+    const x = '__storage_test__'
+    storage.setItem(x, x)
+    storage.removeItem(x)
+    return true
+  } catch (e) {
+    return e instanceof DOMException && (
+      // everything except Firefox
+      e.code === 22 ||
+      // Firefox
+      e.code === 1014 ||
+      // test name field too, because code might not be present
+      // everything except Firefox
+      e.name === 'QuotaExceededError' ||
+      // Firefox
+      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage.length !== 0
+  }
+}
+
 export {
   create2dContext,
   replaceElement,
@@ -122,5 +151,6 @@ export {
   hexToRgb,
   rgbToHex,
   formToJson,
-  getNextIndex
+  getNextIndex,
+  storageAvailable
 }
