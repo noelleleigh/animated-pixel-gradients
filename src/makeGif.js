fa4cbf959e13e1d108d826450706693b7c030e00
Noah Leigh <noah.d.leigh@gmail.com>
2018-03-01 17:10:12 -0500

    Removed commented line

--- src/makeGif.js
+++ src/makeGif.js
@@ -30,7 +30,6 @@ const makeGif = (state, updateFunc, drawFunc, frameDelay, progressHandler, callb
     previousProgress = state.progress
     updateFunc(frameDelay)
     drawFunc()
-    // gif.addFrame(state.ctx, {copy: true, delay: frameDelay})
     gif.addFrame(state.ctxFinal, {copy: true, delay: frameDelay})
   }
   gif.render()
